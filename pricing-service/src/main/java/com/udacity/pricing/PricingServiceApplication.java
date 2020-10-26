49ee7e23f27bb1e2e62e02e332552f2b6fdaa9f7
Hernandez <anhern1@capel0061881147.de080.corpintra.net>
2020-10-26 14:43:26 +0100

    fix update

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -1,12 +1,19 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.repository.PriceRepository;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.LongStream;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
- * TODO: Convert the application from a REST API to a microservice.
  */
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,6 +21,17 @@ public class PricingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PricingServiceApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner init(PriceRepository repository) {
+        return args -> LongStream
+            .range(1, 20).forEach(i -> repository.save(new Price("USD", randomPrice(), i)));
+    }
+
+    private static BigDecimal randomPrice() {
+        return new BigDecimal(ThreadLocalRandom.current().nextDouble(1, 5))
+            .multiply(new BigDecimal(5000d)).setScale(2, RoundingMode.HALF_UP);
     }
 
 }
--- pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,13 +1,22 @@
 package com.udacity.pricing.domain.price;
 
 import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Entity
 public class Price {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
     private String currency;
+
     private BigDecimal price;
     private Long vehicleId;
 
